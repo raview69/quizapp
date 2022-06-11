75594d8594af938a1a5ad7334bcebbbe3bc44d70
raview69 <raviwicaksono@aol.com>
2022-06-12 00:48:51 +0700

    update project.pbxproj and main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,69 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MyApp());
+void main() => runApp(MyApp());
 
+// ignore: use_key_in_widget_constructors
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Material App Bar'),
-        ),
-        body: Column(
-          children: const [
-            Text('data'),
-            RaisedButton(
-              onPressed: null,
-              child: Text('asss'),
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Quizzapp Flutter Ashelole'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  final String title;
+
+  const MyHomePage({
+    Key? key,
+    required this.title,
+  }) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var questions = [
+    'What is your name ?',
+    'What is your age ?',
+    'What is your color ?'
+  ];
+
+  var questionsIndex = 0;
+
+  void onPressed() {
+    setState(() {
+      questionsIndex = questionsIndex + 1;
+    });
+    // ignore: avoid_print
+    print(questionsIndex);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Text(
+              questions[0],
+            ),
+            ElevatedButton(
+              onPressed: onPressed,
+              child: const Text(
+                'Answer',
               ),
+            )
           ],
         ),
       ),
